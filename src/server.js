963716400907081e00e18d0d574224398683a10b
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-07 08:14:44 -0600

    clase 7

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars') //! Requiere en el servidor la función exphbs
 const path = require('path')
 
 //Inicializaciones
@@ -7,6 +8,16 @@ const app = express()
 //Setings
 app.set('port', process.env.PORT || 4000)
 app.set('views', path.join(__dirname, 'views'))
+app.engine(
+	'.hbs',
+	exphbs({
+		defaultLayout: 'main',
+		layoutsDir: path.join(app.get('views'), 'layouts'),
+		partialsDir: path.join(app.get('views'), 'partials'),
+		extname: '.hbs',
+	}),
+) //? Aquí solo se configura
+app.set('view engine', '.hbs') //! Aqui ya se inicializa
 
 //Middlewares
 app.use(express.urlencoded({ extended: false }))
@@ -15,7 +26,7 @@ app.use(express.urlencoded({ extended: false }))
 
 //Routes
 app.get('/', (req, res) => {
-	res.send('Hola mundo')
+	res.render('index')
 })
 
 //Static files
